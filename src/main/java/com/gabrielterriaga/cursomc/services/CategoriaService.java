809ae5c7e1a57b3ef5754276e7bd6c3032c11079
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-08-01 00:28:25 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/gabrielterriaga/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.gabrielterriaga.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielterriaga.cursomc.domain.Categoria;
+import com.gabrielterriaga.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
